79a0187ee76272f4774213bff65c4046620a2b18
Emmanuel Sunday <emas08177@gmail.com>
2026-03-01 00:46:35 +0100

    1.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+import "./assets/css/reset.css";
+import "./assets/css/config.css";
+import "./assets/css/style.css";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import "./assets/css/reset.css";
-import "./assets/css/config.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
