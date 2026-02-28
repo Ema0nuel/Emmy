9bb3b4e1598596ee442a6eeda0762035a1719851
Emmanuel Sunday <emas08177@gmail.com>
2026-03-01 00:36:00 +0100

    0.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AppRoute from "./routes/AppRoute";
+import "./assets/css/reset.css";
 import "./assets/css/style.css";
 
 const App = () => {
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/ui/Navbar";
 import Footer from "../components/ui/Footer";
-import "../assets/css/reset.css";
-import "../assets/css/style.css";
 import useScrollToTop from "../hooks/useScrollToTop";
 
 function MainLayout() {
@@ -10,13 +8,6 @@ function MainLayout() {
 
   return (
     <div className="w-full min-h-screen bg-cover bg-center">
-      <style>{`
-        @media (max-width: 768px) {
-          div {
-            background-attachment: scroll !important;
-          }
-        }
-      `}</style>
       <Navbar />
       <main className="pt-16 md:pt-20 relative z-10 min-h-[calc(100vh-64px)]">
         <Outlet />
